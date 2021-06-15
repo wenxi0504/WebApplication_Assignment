4f8c150f96cbe335e7b6c03a684cd6844482ca3d
wenxi0504 <wxu63@my.centennialcollege.ca>
2021-06-14 21:06:49 -0400

    create Models folder

--- .history/index_20210614202756.js
+++ .history/index_20210614202756.js
@@ -8,12 +8,17 @@ const app_1 = __importDefault(require("./app"));
 const debug_1 = __importDefault(require("debug"));
 debug_1.default('comp229-m2021-assignment2-wenxu-301098127:server');
 const http_1 = __importDefault(require("http"));
+
+/* Get port from environment and store in Express.*/
 let port = normalizePort(process.env.PORT || '3000');
 app_1.default.set('port', port);
 let server = http_1.default.createServer(app_1.default);
+/*Listen on provided port, on all network interfaces.*/
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
+
+/* Normalize a port into a number, string, or false.*/
 function normalizePort(val) {
     let port = parseInt(val, 10);
     if (isNaN(port)) {
@@ -24,6 +29,7 @@ function normalizePort(val) {
     }
     return false;
 }
+/* Event listener for HTTP server "error" event.*/
 function onError(error) {
     if (error.syscall !== 'listen') {
         throw error;
@@ -31,6 +37,7 @@ function onError(error) {
     var bind = typeof port === 'string'
         ? 'Pipe ' + port
         : 'Port ' + port;
+         // handle specific listen errors with friendly messages
     switch (error.code) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges');
@@ -44,6 +51,9 @@ function onError(error) {
             throw error;
     }
 }
+
+/* Event listener for HTTP server "listening" event.*/
+
 function onListening() {
     let addr = server.address();
     let bind = typeof addr === 'string'
--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
-
-/* get method to get home/aboutme/projects/services/contact page*/
 router.get('/', function (req, res, next) {
-    res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Home', page: 'home' });
 });
 router.get('/home', function (req, res, next) {
-    res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Home', page: 'home' });
 });
 router.get('/aboutme', function (req, res, next) {
-    res.render('aboutme', { title: 'About Me' });
+    res.render('aboutme', { title: 'About Me', page: 'about me' });
 });
 router.get('/projects', function (req, res, next) {
-    res.render('projects', { title: 'Projects' });
+    res.render('projects', { title: 'Projects', page: 'projects' });
 });
 router.get('/services', function (req, res, next) {
-    res.render('services', { title: 'Services' });
+    res.render('services', { title: 'Services', page: 'services' });
 });
 router.get('/contact', function (req, res, next) {
-    res.render('contact', { title: 'Contact' });
+    res.render('contact', { title: 'Contact', page: 'contact' });
 });
 //# sourceMappingURL=index.js.map
