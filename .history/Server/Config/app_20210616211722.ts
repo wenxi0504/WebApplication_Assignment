8bec08925698b250179de2bdda1bea0ab3fb0519
wenxi0504 <wxu63@my.centennialcollege.ca>
2021-06-16 21:40:34 -0400

    add User Model

---
+++ .history/Server/Config/app_20210616211722.ts
@@ -0,0 +1,66 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import  Mongoose from 'mongoose';
+
+// attach router file
+import indexRouter from '../../routes/index';
+// a link to contact router
+import contactRouter from '../../routes/contacts_list';
+
+// app configuration
+const app = express();
+export default app; // exports app as the default Object for this module
+
+// db configuration
+import * as DBConfig from './db';
+Mongoose.connect(DBConfig.LocalURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = Mongoose.connection; // alias for the mongoose connection
+db.on("error", function()
+{
+  console.error("connection error");
+});
+
+db.once("open", function()
+{
+  console.log(`Connected to MongoDB at: ${DBConfig.HostName}`);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../../Clients')));
+app.use(express.static(path.join(__dirname, "../../node_modules")));
+
+//rounting happens
+app.use('/', indexRouter);
+//contacts_list is the new area of website
+app.use('/contacts_list', contactRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) 
+{
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err:createError.HttpError, req:express.Request, res:express.Response, next: express.NextFunction) 
+{
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,9 @@ router.get('/about', index_1.DisplayAboutPage);
 router.get('/projects', index_1.DisplayProjectsPage);
 router.get('/services', index_1.DisplayServicesPage);
 router.get('/contact', index_1.DisplayContactPage);
+router.get('/login', DisplayLoginPage);
+router.post('/login', ProcessLoginPage);
+router.get('/register', DisplayRegisterPage);
+router.post('/register', ProcessRegisterPage);
+router.get('/logout', ProcessLogoutPage);
 //# sourceMappingURL=index.js.map
